005a8c9f63b40bbbc45447462dbc442106b207e3
ivoniftrik <ivoniftrik@gmail.com>
2010-07-18 20:00:28 +0000

    (temp) solution for feedback issue 52 - Tabs for episodes list

--- MyEpisodesWatchManagerForAndroid - JAVA/src/eu/vranckaert/episodeWatcher/TabMain.java
+++ MyEpisodesWatchManagerForAndroid - JAVA/src/eu/vranckaert/episodeWatcher/TabMain.java
@@ -9,69 +9,84 @@ import android.widget.TabHost;
 public class TabMain extends TabActivity {
 	private TabHost tabHost;
 	private TabHost.TabSpec spec;
-	private Intent intent;  // Reusable Intent for each tab
+	private Intent intentWatch;  // Reusable Intent for each tab
+	private Intent intentAcquire;  // Reusable Intent for each tab
+	private Intent intentComing;  // Reusable Intent for each tab
 	private Resources res; // Resource object to get Drawables
-	//private TabController tabController;
-	//private EpisodesWatchListActivity episodesWatchListActivity;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	//episodesWatchListActivity = new EpisodesWatchListActivity(tabController);
-    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabmain);
         
         tabHost = getTabHost();  // The activity TabHost
         res = getResources();
-        
         // Create an Intent to launch an Activity for the tab (to be reused)
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 0);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 0);
+        intentWatch = new Intent().setClass(this, EpisodesWatchListActivity.class)
+        					 .putExtra("Type", 0)
+        					 .setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("" + R.string.watch).setIndicator("Watch",
-        res.getDrawable(R.drawable.tabwatched)).setContent(intent);
+        spec = tabHost.newTabSpec("" + R.string.watch)
+        			  .setIndicator("Watch", res.getDrawable(R.drawable.tabwatched))
+        			  .setContent(intentWatch);
         tabHost.addTab(spec);
         
-        // Do the same for the other tabs
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 1);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 1);
-        spec = tabHost.newTabSpec("" + R.string.acquire).setIndicator("Acquire",
-        res.getDrawable(R.drawable.tabacquired)).setContent(intent);
+        intentAcquire = new Intent().setClass(this, EpisodesWatchListActivity.class)
+        					 .putExtra("Type", 1)
+        					 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        spec = tabHost.newTabSpec("" + R.string.acquire)
+        			  .setIndicator("Acquire", res.getDrawable(R.drawable.tabacquired))
+        			  .setContent(intentAcquire);
         tabHost.addTab(spec);
 		
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 2);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 2);
-        spec = tabHost.newTabSpec("" + R.string.coming).setIndicator("Coming",
-        res.getDrawable(R.drawable.tabcoming)).setContent(intent);
+        intentComing = new Intent().setClass(this, EpisodesWatchListActivity.class)
+        					 .putExtra("Type", 2)
+        					 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        spec = tabHost.newTabSpec("" + R.string.coming)
+        			  .setIndicator("Coming", res.getDrawable(R.drawable.tabcoming))
+        			  .setContent(intentComing);
         tabHost.addTab(spec);
+        
+        //tabHost.setCurrentTab(2);
+        //tabHost.setCurrentTab(1);
+        tabHost.setCurrentTab(0);
     }
     
-    public void refreshTabs(int currentTab)
+    public void clearRefreshTab(int episodesType)
     {
-    	tabHost.clearAllTabs();
-    	
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 0);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 0);
-        // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("" + R.string.watch).setIndicator("Watch",
-        res.getDrawable(R.drawable.tabwatched)).setContent(intent);
-        tabHost.addTab(spec);
-        
-        // Do the same for the other tabs
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 1);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 1);
-        spec = tabHost.newTabSpec("" + R.string.acquire).setIndicator("Acquire",
-        res.getDrawable(R.drawable.tabacquired)).setContent(intent);
-        tabHost.addTab(spec);
-        
-        //intent = new Intent().setClass(this, episodesWatchListActivity.getClass()).putExtra("Type", 2);
-        intent = new Intent().setClass(this, EpisodesWatchListActivity.class).putExtra("Type", 2);
-        spec = tabHost.newTabSpec("" + R.string.coming).setIndicator("Coming",
-        res.getDrawable(R.drawable.tabcoming)).setContent(intent);
-        tabHost.addTab(spec);
-        
-        tabHost.setCurrentTab(currentTab);
+    	if (episodesType == 0)
+    	{
+    		intentWatch.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+    	}
+    	else if (episodesType == 1)
+    	{
+    		intentAcquire.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+    	}
+    	else if (episodesType == 2)
+    	{
+    		intentComing.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+    	}
+    }
+    
+    public void refreshAllTabs(int episodesType)
+    {
+    	if (episodesType != 0)
+    	{
+    		intentWatch.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    	}
+    	if (episodesType != 1)
+    	{
+    		intentAcquire.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    	}
+    	if (episodesType != 2)
+    	{
+    		intentComing.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    	}
+    }
+    
+    public void refreshWatchTab()
+    {
+    	intentWatch.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
