fde677e36b2f1ac33cd3c5425cf4f7536d90af78
ivoniftrik <ivoniftrik@cb141f90-d3df-11de-bff8-d51c198f79d0>
2010-08-15 23:10:10 +0000

    Issue 61 - Change language in settings

--- trunk/MyEpisodesWatchManagerForAndroid - JAVA/src/eu/vranckaert/episodeWatcher/PreferencesActivity.java
+++ trunk/MyEpisodesWatchManagerForAndroid - JAVA/src/eu/vranckaert/episodeWatcher/PreferencesActivity.java
@@ -2,19 +2,18 @@ package eu.vranckaert.episodeWatcher;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.text.util.Linkify;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 import eu.vranckaert.episodeWatcher.preferences.Preferences;
 import eu.vranckaert.episodeWatcher.preferences.PreferencesKeys;
 
@@ -25,6 +24,9 @@ import eu.vranckaert.episodeWatcher.preferences.PreferencesKeys;
  */
 public class PreferencesActivity extends PreferenceActivity {
     private static final int RELAOD_DIALOG = 0;
+    private static final int LANGUAGE_DIALOG = RELAOD_DIALOG + 1;
+    private boolean refreshDialog;
+    private boolean languageDialog;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,13 +45,12 @@ public class PreferencesActivity extends PreferenceActivity {
 		return false;
 	}
 
-    private void exit() {
-        super.finish();
-    }
-
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
+        
+        refreshDialog = false;
+        languageDialog = false;
         
         getPreferenceManager().setSharedPreferencesName(Preferences.PREF_NAME);
         setPreferenceScreen(createPreferenceScreen());
@@ -65,6 +66,13 @@ public class PreferencesActivity extends PreferenceActivity {
         root.addPreference(passwordPref);
 
         ListPreference showOrderingPref = new ListPreference(this);
+        showOrderingPref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				refreshDialog = true;
+				return true;
+			}
+        });
         showOrderingPref.setKey(PreferencesKeys.SHOW_SORTING_KEY);
         showOrderingPref.setTitle(R.string.showOrderPrompt);
         showOrderingPref.setEntries(R.array.showOrderOptions);
@@ -72,17 +80,38 @@ public class PreferencesActivity extends PreferenceActivity {
         root.addPreference(showOrderingPref);
 
         ListPreference episodeOrderingPref = new ListPreference(this);
+        episodeOrderingPref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				refreshDialog = true;
+				return true;
+			}
+        });
         episodeOrderingPref.setKey(PreferencesKeys.EPISODE_SORTING_KEY);
         episodeOrderingPref.setTitle(R.string.episodeOrderPrompt);
         episodeOrderingPref.setSummary(R.string.episodeOrderPromptExtra);
         episodeOrderingPref.setEntries(R.array.episodeOrderOptions);
         episodeOrderingPref.setEntryValues(R.array.episodeOrderOptionsValues);
         root.addPreference(episodeOrderingPref);
+        
+        ListPreference showLanguagePref = new ListPreference(this);
+        showLanguagePref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				languageDialog = true;
+				return true;
+			}
+        });
+        showLanguagePref.setKey(PreferencesKeys.LANGUAGE_KEY);
+        showLanguagePref.setTitle(R.string.showLanguagePrompt);
+        showLanguagePref.setEntries(R.array.showLanguageOptions);
+        showLanguagePref.setEntryValues(R.array.showLanguageOptionsValues);
+        root.addPreference(showLanguagePref);
 
         return root;
     }
 
-    @Override
+	@Override
 	protected Dialog onCreateDialog(int id) {
 		Dialog dialog = null;
 		switch (id) {
@@ -99,6 +128,19 @@ public class PreferencesActivity extends PreferenceActivity {
 				       });
 				dialog = builder.create();
 				break;
+			case LANGUAGE_DIALOG:
+				AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+				builder2.setTitle(R.string.attention)
+					   .setMessage(R.string.applyLanguagePreferences)
+					   .setCancelable(false)
+					   .setPositiveButton(R.string.dialogOK, new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				                exit();
+				           }
+				       });
+				dialog = builder2.create();
+				break;
 			default:
 				dialog = super.onCreateDialog(id);
 				break;
@@ -109,6 +151,21 @@ public class PreferencesActivity extends PreferenceActivity {
     @Override
     public void finish() {
         Log.d("PreferencesActivity", "Closing the preferences screen!");
-        showDialog(RELAOD_DIALOG);
+        if (languageDialog)
+        {
+        	showDialog(LANGUAGE_DIALOG);
+        }
+        else if (refreshDialog)
+        {
+        	showDialog(RELAOD_DIALOG);
+        }
+        else
+        {
+        	exit();
+        }
+    }
+    
+    private void exit() {
+    	super.finish();
     }
 }
