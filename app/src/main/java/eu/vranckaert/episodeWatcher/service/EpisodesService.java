384293a8d6e85739b94fe61c9923e108e1d63a00
Dirk Vranckaert <dirkvranckaert@gmail.com>
2015-10-27 16:50:37 +0100

    Revert fix that didn't work :-)

--- app/src/main/java/eu/vranckaert/episodeWatcher/service/EpisodesService.java
+++ app/src/main/java/eu/vranckaert/episodeWatcher/service/EpisodesService.java
@@ -53,7 +53,6 @@ public class EpisodesService {
 
 	public HttpClient getHttpClient() {
 		DefaultHttpClient httpClient = new DefaultHttpClient();
-		httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
 		return httpClient;
 	}
 
